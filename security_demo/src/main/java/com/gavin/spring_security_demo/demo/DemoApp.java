23bd05e870b865db4d5cd0fc8fe9282fef0d8cee
liudonglin <454812428@qq.com>
2018-11-12 23:28:45 +0800

    update

---
+++ security_demo/src/main/java/com/gavin/spring_security_demo/demo/DemoApp.java
@@ -0,0 +1,13 @@
+package com.gavin.spring_security_demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class,args);
+    }
+
+}
